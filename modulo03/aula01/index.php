14a06a5dad98b446de3252e3185f7b8cfe0e041a
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-05-20 11:40:14 -0300

    .

--- modulo03/aula01/index.php
+++ modulo03/aula01/index.php
@@ -1,11 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
 </head>
+
 <body>
-    
+	<?php
+	class pessoa
+	{
+
+		private $nome = 'Gabriel';
+		private $idade = '23';
+		private $peso = '67Kg';
+
+		public function crescer()
+		{
+			echo 'estou crescendo';
+			$this->comer();
+		}
+		public function comer()
+		{
+			echo ' estou comendo';
+		}
+
+	}//pessoa
+	$pessoa = new pessoa;
+	$pessoa2 = new pessoa;
+
+	$pessoa->crescer();
+
+	?><!--php-->
 </body>
+
 </html>
