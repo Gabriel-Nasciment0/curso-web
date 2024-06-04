4c84e29e9608ba4969bbac8b38ae59246f033541
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-06-04 07:32:33 -0300

    s

--- modulo04/aula02/index.php
+++ modulo04/aula02/index.php
@@ -6,7 +6,7 @@ if (isset($_POST['acao'])) {
     $nome = $_POST['nome'];
     $sobrenome = $_POST['sobrenome'];
     $momento_registro = date('Y-m-d H:i:s');
-    $sql = $pdo->prepare("INSERT INTO `clientes` VALUES (null,?,?,?)");
+    $sql = $pdo->prepare("INSERT INTO `clientes` VALUES (,?,?,?)");
     $sql->execute(array($nome, $sobrenome, $momento_registro));
     echo "cliente inserido";
 }
