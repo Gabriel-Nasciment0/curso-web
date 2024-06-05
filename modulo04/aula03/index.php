e5720cb2e21f5a7f8ca9ca07066d3f7958d5b84e
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-06-05 19:07:00 -0300

    s

--- modulo04/aula03/index.php
+++ modulo04/aula03/index.php
@@ -1,6 +1,6 @@
-<?php 
-    $pdo = new PDO('mysql:host=localhost;dbname=modulo04','root','');
-   
-    $id
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=modulo04', 'root', '');
 
-?>
+$id = 10;
+
+$sql = $pdo->prepare("UPDATE `clientes` SET nome='Luiz',sobrenome='Gustavo' WHERE id=$id");
