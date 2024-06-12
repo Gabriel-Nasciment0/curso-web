d58071f5b60f472a5c4a22c1d3b7a7da671cd07b
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-06-12 19:21:32 -0300

    g

--- modulo05 projeto01/index.php
+++ modulo05 projeto01/index.php
@@ -8,7 +8,49 @@
 </head>
 
 <body>
-    Ola mundo<h1></h1>
+
+    <header>
+        <div class="logo">Logomarca
+
+        </div><!--logo-->
+        <nav class="desktop">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Serviçoes</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav><!--desktop-->
+        <nav class="mobile">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Serviçoes</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav><!--desktop-->
+    </header>
+    <section class="banner_principal">
+        <form action="">
+            <h2>Qual seu melhor email</h2>
+            <input type="email" name="email" required>
+            <input type="submit" name="acao" value="Cadastrar">
+        </form>
+    </section><!--banner-->
+    <section class="descricao_autor">
+        <div class="w50">
+
+            <h2>Gabriel Nasciment0</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, facere sapiente aliquam dolore blanditiis veritatis harum velit doloremque id at. Doloremque nulla blanditiis error nostrum architecto nihil labore, a repellendus dicta voluptas tempore alias iure. Incidunt inventore ad officia voluptatum quaerat ex eius, id dolores explicabo earum, labore quisquam, cupiditate necessitatibus totam! Placeat, nostrum! Reiciendis quod quaerat dolor labore blanditiis officia iusto at possimus laboriosam eveniet eum fugit placeat praesentium voluptatum dicta odio ipsa, obcaecati ullam voluptate eius distinctio laborum cumque similique ipsam? Sapiente, quas sint ipsam aliquam animi voluptas labore velit repudiandae autem nisi dolore natus ipsum molestiae aut.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, facere sapiente aliquam dolore blanditiis veritatis harum velit doloremque id at. Doloremque nulla blanditiis error nostrum architecto nihil labore, a repellendus dicta voluptas tempore alias iure. Incidunt inventore ad officia voluptatum quaerat ex eius, id dolores explicabo earum, labore quisquam, cupiditate necessitatibus totam! Placeat, nostrum! Reiciendis quod quaerat dolor labore blanditiis officia iusto at possimus laboriosam eveniet eum fugit placeat praesentium voluptatum dicta odio ipsa, obcaecati ullam voluptate eius distinctio laborum cumque similique ipsam? Sapiente, quas sint ipsam aliquam animi voluptas labore velit repudiandae autem nisi dolore natus ipsum molestiae aut.</p>
+        </div><!--w50-->
+        <div class="wd50">
+            <!-- pegar imagem depois -->
+            <img src="https://images.pexels.com/photos/18820290/pexels-photo-18820290/free-photo-of-natureza-floresta-selva-mata.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="">
+        </div><!--wd50-->
+
+    </section><!--descricao-->
+
 </body>
 
 </html>
