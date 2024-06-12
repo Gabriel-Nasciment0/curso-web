eaf0673a8ed595027df748ce2c5073399e0f3684
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-06-12 19:46:43 -0300

    .

--- modulo05 projeto01/index.php
+++ modulo05 projeto01/index.php
@@ -10,46 +10,148 @@
 <body>
 
     <header>
-        <div class="logo">Logomarca
+        <div class="center">
 
-        </div><!--logo-->
-        <nav class="desktop">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Serviçoes</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-        </nav><!--desktop-->
-        <nav class="mobile">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Serviçoes</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-        </nav><!--desktop-->
+
+            <div class="logo">Logomarca
+
+            </div><!--logo-->
+            <nav class="desktop">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviçoes</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav><!--desktop-->
+            <nav class="mobile">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Serviçoes</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav><!--desktop-->
+        </div><!--center-->
     </header>
     <section class="banner_principal">
-        <form action="">
-            <h2>Qual seu melhor email</h2>
-            <input type="email" name="email" required>
-            <input type="submit" name="acao" value="Cadastrar">
-        </form>
+        <div class="center">
+            <form action="">
+                <h2>Qual seu melhor email</h2>
+                <input type="email" name="email" required>
+                <input type="submit" name="acao" value="Cadastrar">
+            </form>
+
+        </div><!--center-->
     </section><!--banner-->
     <section class="descricao_autor">
-        <div class="w50">
+        <div class="center">
+            <div class="w50">
 
-            <h2>Gabriel Nasciment0</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, facere sapiente aliquam dolore blanditiis veritatis harum velit doloremque id at. Doloremque nulla blanditiis error nostrum architecto nihil labore, a repellendus dicta voluptas tempore alias iure. Incidunt inventore ad officia voluptatum quaerat ex eius, id dolores explicabo earum, labore quisquam, cupiditate necessitatibus totam! Placeat, nostrum! Reiciendis quod quaerat dolor labore blanditiis officia iusto at possimus laboriosam eveniet eum fugit placeat praesentium voluptatum dicta odio ipsa, obcaecati ullam voluptate eius distinctio laborum cumque similique ipsam? Sapiente, quas sint ipsam aliquam animi voluptas labore velit repudiandae autem nisi dolore natus ipsum molestiae aut.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, facere sapiente aliquam dolore blanditiis veritatis harum velit doloremque id at. Doloremque nulla blanditiis error nostrum architecto nihil labore, a repellendus dicta voluptas tempore alias iure. Incidunt inventore ad officia voluptatum quaerat ex eius, id dolores explicabo earum, labore quisquam, cupiditate necessitatibus totam! Placeat, nostrum! Reiciendis quod quaerat dolor labore blanditiis officia iusto at possimus laboriosam eveniet eum fugit placeat praesentium voluptatum dicta odio ipsa, obcaecati ullam voluptate eius distinctio laborum cumque similique ipsam? Sapiente, quas sint ipsam aliquam animi voluptas labore velit repudiandae autem nisi dolore natus ipsum molestiae aut.</p>
-        </div><!--w50-->
-        <div class="wd50">
-            <!-- pegar imagem depois -->
-            <img src="https://images.pexels.com/photos/18820290/pexels-photo-18820290/free-photo-of-natureza-floresta-selva-mata.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="">
-        </div><!--wd50-->
+                <h2>Gabriel Nasciment0</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, facere sapiente aliquam dolore
+                    blanditiis veritatis harum velit doloremque id at. Doloremque nulla blanditiis error nostrum
+                    architecto nihil labore, a repellendus dicta voluptas tempore alias iure. Incidunt inventore ad
+                    officia voluptatum quaerat ex eius, id dolores explicabo earum, labore quisquam, cupiditate
+                    necessitatibus totam! Placeat, nostrum! Reiciendis quod quaerat dolor labore blanditiis officia
+                    iusto at possimus laboriosam eveniet eum fugit placeat praesentium voluptatum dicta odio ipsa,
+                    obcaecati ullam voluptate eius distinctio laborum cumque similique ipsam? Sapiente, quas sint ipsam
+                    aliquam animi voluptas labore velit repudiandae autem nisi dolore natus ipsum molestiae aut.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, facere sapiente aliquam dolore
+                    blanditiis veritatis harum velit doloremque id at. Doloremque nulla blanditiis error nostrum
+                    architecto nihil labore, a repellendus dicta voluptas tempore alias iure. Incidunt inventore ad
+                    officia voluptatum quaerat ex eius, id dolores explicabo earum, labore quisquam, cupiditate
+                    necessitatibus totam! Placeat, nostrum! Reiciendis quod quaerat dolor labore blanditiis officia
+                    iusto at possimus laboriosam eveniet eum fugit placeat praesentium voluptatum dicta odio ipsa,
+                    obcaecati ullam voluptate eius distinctio laborum cumque similique ipsam? Sapiente, quas sint ipsam
+                    aliquam animi voluptas labore velit repudiandae autem nisi dolore natus ipsum molestiae aut.</p>
+            </div><!--w50-->
+            <div class="wd50">
+                <!-- pegar imagem depois -->
+                <img src="https://images.pexels.com/photos/18820290/pexels-photo-18820290/free-photo-of-natureza-floresta-selva-mata.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="">
+            </div><!--wd50-->
 
+
+        </div><!--center-->
     </section><!--descricao-->
+    <section class="especialidades">
+        <h2 class="title">especialidades</h2>
+        <div class="center">
+            <div class="box_especialidade">
+                <h3>icone01</h3>
+                <h3>CSS3</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ab tenetur quas vitae perferendis itaque
+                    in, tempora ut accusantium quibusdam voluptas cumque ipsum ipsa ducimus minima obcaecati soluta
+                    deleniti. Facere.</p>
+            </div><!--especialidade-->
+            <div class="box_especialidade">
+                <h3>icone02</h3>
+                <h3>HTML05</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ab tenetur quas vitae perferendis itaque
+                    in, tempora ut accusantium quibusdam voluptas cumque ipsum ipsa ducimus minima obcaecati soluta
+                    deleniti. Facere.</p>
+            </div><!--especialidade-->
+            <div class="box_especialidade">
+                <h3>icone03</h3>
+                <h3>JavaScript</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ab tenetur quas vitae perferendis itaque
+                    in, tempora ut accusantium quibusdam voluptas cumque ipsum ipsa ducimus minima obcaecati soluta
+                    deleniti. Facere.</p>
+            </div><!--especialidade-->
+        </div><!--center-->
+    </section><!--especialidades-->
+    <section class="extras">
+        <div class="center">
+            <div class="w50">
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento_single">
+                    <p class="depoimento_descricao">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+                        voluptas commodi mollitia corporis voluptate ea molestias est consequatur reiciendis officia
+                        incidunt et assumenda consequuntur dicta, excepturi autem tempora cupiditate eveniet.</p>
+                    <p class="nome_autor">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A rem dolorem
+                        debitis nobis et dolorum quia sit ex culpa aliquam tempora magnam voluptatem vitae quisquam,
+                        corrupti laudantium, ullam corporis. A?</p>
+                </div><!--single-->
+                <div class="depoimento_single">
+                    <p class="depoimento_descricao">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+                        voluptas commodi mollitia corporis voluptate ea molestias est consequatur reiciendis officia
+                        incidunt et assumenda consequuntur dicta, excepturi autem tempora cupiditate eveniet.</p>
+                    <p class="nome_autor">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A rem dolorem
+                        debitis nobis et dolorum quia sit ex culpa aliquam tempora magnam voluptatem vitae quisquam,
+                        corrupti laudantium, ullam corporis. A?</p>
+                </div><!--single-->
+                <div class="depoimento_single">
+                    <p class="depoimento_descricao">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+                        voluptas commodi mollitia corporis voluptate ea molestias est consequatur reiciendis officia
+                        incidunt et assumenda consequuntur dicta, excepturi autem tempora cupiditate eveniet.</p>
+                    <p class="nome_autor">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A rem dolorem
+                        debitis nobis et dolorum quia sit ex culpa aliquam tempora magnam voluptatem vitae quisquam,
+                        corrupti laudantium, ullam corporis. A?</p>
+                </div><!--single-->
+            </div><!--w50-->
+            <div class="w50">
+                <h2 class="title">Serviçoes</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, officiis distinctio.
+                            Tenetur magni ratione officia, unde dicta facere optio qui eveniet voluptatum beatae sit
+                            eligendi? Reprehenderit inventore ducimus accusantium voluptate.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, officiis distinctio.
+                            Tenetur magni ratione officia, unde dicta facere optio qui eveniet voluptatum beatae sit
+                            eligendi? Reprehenderit inventore ducimus accusantium voluptate.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, officiis distinctio.
+                            Tenetur magni ratione officia, unde dicta facere optio qui eveniet voluptatum beatae sit
+                            eligendi? Reprehenderit inventore ducimus accusantium voluptate.</li>
+                    </ul>
+                </div><!--servicos-->
+            </div><!--w50-->
+        </div><!--center-->
+    </section><!--extras-->
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados</p>
+        </div><!--center-->
+    </footer>
 
 </body>
 
