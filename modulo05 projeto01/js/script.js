175560b36e069619c7649a3ea766e683522fbc4e
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-06-20 18:26:25 -0300

    3

--- modulo05 projeto01/js/script.js
+++ modulo05 projeto01/js/script.js
@@ -1,5 +1,30 @@
 $(function () {
     $("nav.mobile").click(function () {
-        alert("funciona");
+        var listaMenu = $("nav.mobile ul");
+        // abrir menu com fade in
+        // if (listaMenu.is(":hidden") == true) {
+        //     listaMenu.fadeIn();
+        // } else {
+        //     listaMenu.fadeOut();
+        // }
+        // abrir menu com slide toggle
+
+        // if (listaMenu.is(":hidden") == true) {
+        //     listaMenu.css("display", "block");
+        // } else {
+        //     listaMenu.css("display", "none");
+        // }
+        if (listaMenu.is(":hidden") == true) {
+            var icone = $(".botao_menu_mobile").find("i");
+            icone.removeClass("fa-solid fa-bars");
+            icone.addClass("fa-solid fa-x");
+            listaMenu.slideToggle();
+        } else {
+            var icone = $(".botao_menu_mobile").find("i");
+
+            icone.removeClass("fa-solid fa-x");
+            icone.addClass("fa-solid fa-bars");
+            listaMenu.slideToggle();
+        }
     });
 });
