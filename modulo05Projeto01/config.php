3b606bb2fa96c02e8abf58300d6517ae287d2f84
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-07-03 14:16:21 -0300

    3

--- modulo05Projeto01/config.php
+++ modulo05Projeto01/config.php
@@ -1,3 +1,8 @@
 <?php
 
+$autoload = function ($class) {
+
+    include('classes/' . $class . '.php');
+};
+spl_autoload_register($autoload);
 define('INCLUDE_PATH', 'http://localhost/GitHub/curso-web/modulo05Projeto01/');
