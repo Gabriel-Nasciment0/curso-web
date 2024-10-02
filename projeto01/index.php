e58e6013b26317c41b606e1ca6ae3744e096b26a
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-10-02 10:07:31 -0300

    v

---
+++ projeto01/index.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>Projeto01</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- SEO meta tags -->
+    <meta name="keywords" content="palavras-chave,do,meu,site">
+    <meta name="description" content="Descrição do meu site">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <header>
+        <div class="center">
+
+            <div class="logo">Logomarca</div><!--logo-->
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav><!--desktop-->
+            <nav class="mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav><!--mobile-->
+        </div><!--center-->
+    </header>
+
+    <section class="banner-principal">
+        <div class="center">
+
+            <form>
+                <h2>Qual o seu e-mail</h2>
+                <input type="email" name="email" required>
+                <input type="submit" name="acao" value="Cadastrar">
+            </form>
+        </div><!--center-->
+    </section><!--banner-principal-->
+
+    <section class="descricao-autor">
+        <div class="center">
+
+            <div class="w50">
+                <h2>Gabriel Nasciment0</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto blanditiis ratione odio perspiciatis molestias totam animi harum rerum sequi quae, a, vitae est aspernatur voluptas quos, eos eligendi facilis repudiandae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime obcaecati aut officia architecto alias esse, facere blanditiis, fuga ducimus repellendus molestias voluptatibus aliquam porro, iste commodi corporis facilis ea accusamus!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto blanditiis ratione odio perspiciatis molestias totam animi harum rerum sequi quae, a, vitae est aspernatur voluptas quos, eos eligendi facilis repudiandae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime obcaecati aut officia architecto alias esse, facere blanditiis, fuga ducimus repellendus molestias voluptatibus aliquam porro, iste commodi corporis facilis ea accusamus!</p>
+
+            </div><!--w50-->
+        </div><!--center-->
+        <div class="w50">
+            <!-- pegar imagem depois -->
+            <img src="images\foto.jpg">
+        </div><!--w50-->
+    </section><!--descricao-autor-->
+
+    <section class="especialidades">
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <div class="box-especialidade">
+                <h3>icone1</h3>
+                <h3>CSS3</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum similique obcaecati ipsam fugit omnis adipisci explicabo debitis et perspiciatis, veritatis amet, facilis fugiat id excepturi, impedit nobis eligendi? Fugit?</p>
+            </div><!--box-especialidade-->
+            <div class="box-especialidade">
+                <h3>icone2</h3>
+                <h3>HTML5</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum similique obcaecati ipsam fugit omnis adipisci explicabo debitis et perspiciatis, veritatis amet, facilis fugiat id excepturi, impedit nobis eligendi? Fugit?</p>
+            </div><!--box-especialidade-->
+            <div class="box-especialidade">
+                <h3>icone3</h3>
+                <h3>JavaScript</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum similique obcaecati ipsam fugit omnis adipisci explicabo debitis et perspiciatis, veritatis amet, facilis fugiat id excepturi, impedit nobis eligendi? Fugit?</p>
+            </div><!--box-especialidade-->
+        </div><!--center-->
+    </section><!--especialidades-->
+
+    <section class="extras">
+        <div class="center">
+            <div class="w50">
+
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero culpa aliquam quod laudantium qui sit explicabo earum expedita repellendus optio blanditiis dignissimos, inventore, omnis repellat molestiae magnam tenetur quam reiciendis.</p>
+                    <p class="nome-autor">lorem ipsum</p>
+                </div><!--depoimento-single-->
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero culpa aliquam quod laudantium qui sit explicabo earum expedita repellendus optio blanditiis dignissimos, inventore, omnis repellat molestiae magnam tenetur quam reiciendis.</p>
+                    <p class="nome-autor">lorem ipsum</p>
+                </div><!--depoimento-single-->
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero culpa aliquam quod laudantium qui sit explicabo earum expedita repellendus optio blanditiis dignissimos, inventore, omnis repellat molestiae magnam tenetur quam reiciendis.</p>
+                    <p class="nome-autor">lorem ipsum</p>
+                </div><!--depoimento-single-->
+            </div><!--w50-->
+            <div class="w50">
+
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam iure, provident repudiandae dolorem minus sequi necessitatibus asperiores quis maxime officia at quos error eveniet sit aperiam facilis aliquid eaque!</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam iure, provident repudiandae dolorem minus sequi necessitatibus asperiores quis maxime officia at quos error eveniet sit aperiam facilis aliquid eaque!</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam iure, provident repudiandae dolorem minus sequi necessitatibus asperiores quis maxime officia at quos error eveniet sit aperiam facilis aliquid eaque!</li>
+                    </ul>
+                </div><!--servicos-->
+            </div><!--w50-->
+        </div><!--center-->
+    </section><!--extras-->
+
+
+
+    <footer>
+        <div class="center">
+
+            <p>Todos os direitos reservados</p>
+        </div><!--center-->
+    </footer>
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
