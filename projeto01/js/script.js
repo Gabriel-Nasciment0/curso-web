90f0bf35aebf70c02e0b80a7210fe949d3a3dc7b
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-10-16 09:28:22 -0300

    .

--- projeto01/js/script.js
+++ projeto01/js/script.js
@@ -2,5 +2,13 @@ $(function () {
     $(".botao-menu-mobile").click(function () {
         var listaMenu = $("nav.mobile ul");
         listaMenu.slideToggle();
+
+        // Alternar entre ícone de menu e de fechar
+        var icon = $(".botao-menu-mobile i");
+        if (icon.hasClass("fa-bars")) {
+            icon.removeClass("fa-bars").addClass("fa-x");
+        } else {
+            icon.removeClass("fa-x").addClass("fa-bars");
+        }
     });
 });
