074dc7f75dedcc3cffb16d1cf9c1eae532ec9fc0
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-10-21 09:20:10 -0300

    .

---
+++ projeto01/pages/err404.php
@@ -0,0 +1,9 @@
+<section class="err404">
+    <div class="center">
+        <div class="wraper-404">
+
+            <h1><i class="fa-solid fa-x"></i> A pagina não existe</h1>
+            <p>Deseja voltar a <a href=" <?php echo INCLUDE_PATH ?>">pagina inicial?</a></p>
+        </div>
+    </div>
+</section>
---
+++ projeto01/pages/home.php
@@ -0,0 +1,83 @@
+<section class="banner-principal">
+    <div class="overlay"></div>
+    <div class="center">
+
+        <form>
+            <h2>Qual o seu e-mail</h2>
+            <input type="email" name="email" required>
+            <input type="submit" name="acao" value="Cadastrar">
+        </form>
+    </div><!--center-->
+</section><!--banner-principal-->
+
+<section class="descricao-autor">
+    <div class="center">
+
+        <div class="w50 left">
+            <h2>Gabriel Nasciment0</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto blanditiis ratione odio perspiciatis molestias totam animi harum rerum sequi quae, a, vitae est aspernatur voluptas quos, eos eligendi facilis repudiandae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime obcaecati aut officia architecto alias esse, facere blanditiis, fuga ducimus repellendus molestias voluptatibus aliquam porro, iste commodi corporis facilis ea accusamus!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto blanditiis ratione odio perspiciatis molestias totam animi harum rerum sequi quae, a, vitae est aspernatur voluptas quos, eos eligendi facilis repudiandae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime obcaecati aut officia architecto alias esse, facere blanditiis, fuga ducimus repellendus molestias voluptatibus aliquam porro, iste commodi corporis facilis ea accusamus!</p>
+
+        </div><!--w50-->
+    </div><!--center-->
+    <div class="w50 left">
+
+        <img class="right" src="<?php INCLUDE_PATH; ?>images/foto.jpg">
+    </div><!--w50-->
+    <div class="clear"></div>
+</section><!--descricao-autor-->
+
+<section class="especialidades">
+    <div class="center">
+        <h2 class="title">Especialidades</h2>
+        <div class=" w33 left box-especialidade">
+            <h3><i class="fa-brands fa-css3"></i></h3>
+            <h4>CSS3</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum similique obcaecati ipsam fugit omnis adipisci explicabo debitis et perspiciatis, veritatis amet, facilis fugiat id excepturi, impedit nobis eligendi? Fugit?</p>
+        </div><!--box-especialidade-->
+        <div class="w33 left box-especialidade">
+            <h3><i class="fa-brands fa-html5"></i></h3>
+            <h4>HTML5</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum similique obcaecati ipsam fugit omnis adipisci explicabo debitis et perspiciatis, veritatis amet, facilis fugiat id excepturi, impedit nobis eligendi? Fugit?</p>
+        </div><!--box-especialidade-->
+        <div class="w33 left box-especialidade">
+            <h3><i class="fa-brands fa-js"></i></h3>
+            <h4>JavaScript</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque laborum similique obcaecati ipsam fugit omnis adipisci explicabo debitis et perspiciatis, veritatis amet, facilis fugiat id excepturi, impedit nobis eligendi? Fugit?</p>
+        </div><!--box-especialidade-->
+        <div class="clear"></div>
+    </div><!--center-->
+</section><!--especialidades-->
+
+<section class="extras">
+    <div class="center">
+        <div class="w50 left depoimentos-container">
+
+            <h2 class="title">Depoimentos dos nossos clientes</h2>
+            <div class="depoimento-single">
+                <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero culpa aliquam quod laudantium qui sit explicabo earum expedita repellendus optio blanditiis dignissimos, inventore, omnis repellat molestiae magnam tenetur quam reiciendis.</p>
+                <p class="nome-autor">lorem ipsum</p>
+            </div><!--depoimento-single-->
+            <div class="depoimento-single">
+                <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero culpa aliquam quod laudantium qui sit explicabo earum expedita repellendus optio blanditiis dignissimos, inventore, omnis repellat molestiae magnam tenetur quam reiciendis.</p>
+                <p class="nome-autor">lorem ipsum</p>
+            </div><!--depoimento-single-->
+            <div class="depoimento-single">
+                <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero culpa aliquam quod laudantium qui sit explicabo earum expedita repellendus optio blanditiis dignissimos, inventore, omnis repellat molestiae magnam tenetur quam reiciendis.</p>
+                <p class="nome-autor">lorem ipsum</p>
+            </div><!--depoimento-single-->
+        </div><!--w50-->
+        <div class="w50 left servicos-container">
+
+            <h2 class="title">Serviços</h2>
+            <div class="servicos">
+                <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam iure, provident repudiandae dolorem minus sequi necessitatibus asperiores quis maxime officia at quos error eveniet sit aperiam facilis aliquid eaque!</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam iure, provident repudiandae dolorem minus sequi necessitatibus asperiores quis maxime officia at quos error eveniet sit aperiam facilis aliquid eaque!</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laboriosam iure, provident repudiandae dolorem minus sequi necessitatibus asperiores quis maxime officia at quos error eveniet sit aperiam facilis aliquid eaque!</li>
+                </ul>
+            </div><!--servicos-->
+        </div><!--w50-->
+        <div class="clear"></div>
+    </div><!--center-->
+</section><!--extras-->
