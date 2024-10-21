f245c0fb3e95e41dd091a3a2ccdeb5fd72500e0a
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-10-21 10:24:56 -0300

    .

---
+++ projeto01/pages/contato.php
@@ -0,0 +1,38 @@
+<div class="map">
+    <div class="mapouter">
+        <div class="gmap_canvas"><iframe width="100%" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=636+5th+Ave%2C+New+York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.alarm-clock.net/">online alarm clock</a><br><a href="https://timenowin.net/"></a><br>
+            <style>
+                .mapouter {
+                    position: relative;
+                    text-align: right;
+                    height: 400px;
+                    width: 100%;
+                }
+            </style><a href="https://www.embedmaps.co">map box</a>
+            <style>
+                .gmap_canvas {
+                    overflow: hidden;
+                    background: none !important;
+                    height: 400px;
+                    width: 100%;
+                }
+            </style>
+        </div>
+    </div>
+</div>
+
+<div class="contato-container">
+    <div class="center">
+        <form>
+            <input type="text" name="nome" placeholder="Nome...">
+            <div></div>
+            <input type="text" name="email" placeholder="E-mail...">
+            <div></div>
+            <input type="text" name="telefone" placeholder="Telefone...">
+            <div></div>
+            <textarea name="mensagem"></textarea>
+            <div></div>
+            <input type="submit" name="acao" value="Enviar">
+        </form>
+    </div><!--center-->
+</div><!--contato-container-->
