a7d456bf91b871621fa940df91c4a2209b8be44e
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-11-05 17:16:28 -0300

    .

--- projeto01/config.php
+++ projeto01/config.php
@@ -2,9 +2,9 @@
 // envio de email 
 $autoLoad = function ($class) {
     if ($class == 'Email') {
-        include('classes/vendor/autoload.php');
+        include('classes/PHPMailer/PHPMailerAutoload.php');
     }
-    include('classes/phpmailer/' . $class . '.php');
+    include('classes/' . $class . '.php');
 };
 spl_autoload_register($autoLoad);
 // vincular aruivo ao diretorio principal
